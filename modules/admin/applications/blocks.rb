7458c688fe03c253d915174d365df37eb83924fd
coolesting <coolesting@gmail.com>
2012-06-01 02:04:17 -0700

    complete the #opt, and block adding

--- modules/admin/applications/blocks.rb
+++ modules/admin/applications/blocks.rb
@@ -1,11 +1,20 @@
 get '/admin/blocks' do
-	opt_events :add, :remove, :alter
+	opt_events :new
 	@title += ' the system blocks'
 	@blocks = DB[:blocks]
 	slim :blocks
 end
 
 get '/admin/blocks/new' do
-	set :msg, 'Add a new block'
+	opt_events :save
 	slim :admin_block_new
 end
+
+post '/admin/blocks/new' do
+	str = ""
+	params.each do | k, v |
+		str += "#{k} : #{v} <br>"
+	end
+	str
+end
+
--- modules/front/applications/routors.rb
+++ modules/front/applications/routors.rb
@@ -2,13 +2,6 @@ get '/' do
 	slim :front
 end
 
-get "/set" do
-	iset :time_now, Time.now
-	@time_now = Time.now
-	slim :front
-end
-
-get "/get" do
-	@time_now = iget :time_now
-	slim :front
+post "/form1" do
+	params[:f1] + params[:f2] + params[:opt]
 end
