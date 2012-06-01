e04dcd2ab42c8b9e809cb9a903cd24b4aca4a497
coolesting <coolesting@gmail.com>
2012-05-31 18:40:10 -0700

    alter the title for display

--- modules/admin/applications/routors.rb
+++ modules/admin/applications/routors.rb
@@ -1,24 +1,21 @@
 get '/admin/modules' do
-	@title += ' the system modules'
+	@title += ' - the system modules'
 	slim :modules
 end
 
 get '/admin/settings' do
-	@title += ' the system settings'
+	@title += ' - the system settings'
 	@settings = DB[:settings]
 	slim :settings
 end
 
 get '/admin/info' do
-	@title += ' the back ground page'
+	@title += ' - the back ground page'
 	slim :admin
 end
 
 get '/admin/info/t' do
-	@title += ' the back ground page'
+	@title += ' - the back ground page'
 	slim :admin
 end
 
-configure do
-	set :login_time, Time.now
-end
