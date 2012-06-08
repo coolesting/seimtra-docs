99e7e329997d4b27e1e429953db8ffd8da0b48c0
coolesting <coolesting@gmail.com>
2012-06-07 19:08:46 -0700

    alter the router and template of panel

--- modules/system/applications/panel.rb
+++ modules/system/applications/panel.rb
@@ -5,69 +5,75 @@ end
 
 get '/system/panel/new' do
 	opt_events :save
-	panel_init_fields
+	panel_process_fields
 	slim :system_panel_form
 end
 
 get '/system/panel/edit/:pid' do
 
-	opt_events :save
+	opt_events :save, :remove
 	@fields = DB[:panel].filter(:pid => params[:pid]).all[0]
 
 # 	@fields = DB[:panel].select(:pid, :menu, :name, :link, :description, :status, :order).filter(:pid => params[:pid]).all[0]
 
- 	panel_init_fields
+ 	panel_process_fields
  	slim :system_panel_form
 
 end
 
 post '/system/panel/new' do
 
-	panel_init_fields
-	if @fields[:menu] != "" and @fields[:name] != "" and @fields[:link] != ""
-		@fields.delete :pid
-		DB[:panel].insert(@fields)
-		redirect "/system/panel"
-	else
-		send_error 0
-	end
+	panel_process_fields :valid
+	@fields.delete :pid
+	DB[:panel].insert(@fields)
+	redirect "/system/panel"
 
 end
 
 post '/system/panel/edit/:pid' do
 
-	panel_init_fields
-	if @fields[:menu] != "" and @fields[:name] != "" and @fields[:link] != "" and @fields[:pid].to_i > 0
-		if DB[:panel][:pid => @fields[:pid]][:pid]
-			id = @fields.delete :pid
-			DB[:panel].filter(:pid => id.to_i).update(@fields)
-			redirect "/system/panel"
-		end
-	else
-		send_error 0
+	panel_process_fields :valid
+
+	if params[:opt] == "Remove"
+		DB[:panel].delete(:pid => @fields[:pid])
+	elsif params[:opt] == "Save"
+		id = @fields.delete :pid
+		DB[:panel].filter(:pid => id.to_i).update(@fields)
 	end
+	redirect "/system/panel"
+
 
 end
 
 helpers do
 
-	def panel_init_fields
+	def panel_process_fields opt = :init, data = {}
 		
-		default_values = {
-			:name		=> "",
-			:link		=> "",
-			:menu		=> "",
-			:description=> "",
-			:status		=> 0,
-			:order		=> 1
-		}
+		if opt == :valid
 
-		valid_pattens = {}
+			valid_pattens = {}
 
-		default_values.each do | k, v |
-			unless @fields.include? k
-				@fields[k] = params[k] ? params[k] : v
+			if DB[:panel][:pid => @fields[:pid]][:pid]
 			end
+		
+		elsif opt == :init
+
+			default_values = {
+				:name		=> "",
+				:link		=> "",
+				:menu		=> "",
+				:description=> "",
+				:status		=> 0,
+				:order		=> 1
+			}
+
+
+			default_values.each do | k, v |
+				unless @fields.include? k
+					@fields[k] = params[k] ? params[k] : v
+				end
+			end
+
 		end
 
 	end
