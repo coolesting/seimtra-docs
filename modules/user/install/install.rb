f9cd8b8a885d39a1e14776363a1bad83c10834e9
coolesting <coolesting@gmail.com>
2012-08-15 19:12:54 -0700

    update the install method

--- modules/user/install/install.rb
+++ modules/user/install/install.rb
@@ -1,15 +1,13 @@
 class Seimtra_system
 
-	def system_add_user data
+	def preprocess_user data
+		require "digest/sha1"
 		data.each do | row |
-			require "digest/sha1"
-
 			row[:pawd] 		= Digest::SHA1.hexdigest(row[:pawd] + row[:salt])
 			row[:created] 	= Time.now
 			row[:changed] 	= Time.now
-
-			DB[:user].insert(row)
 		end
+		data
 	end
 
 end
