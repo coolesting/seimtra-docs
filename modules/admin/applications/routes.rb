5ff0f5046dc120bd655290caab9586423b5095c0
coolesting <coolesting@gmail.com>
2012-12-10 18:18:25 -0800

    update template of module

--- modules/admin/applications/routes.rb
+++ modules/admin/applications/routes.rb
@@ -10,7 +10,8 @@ get '/admin/module' do
 	sys_tpl :admin_module
 end
 
-get '/admin/module/off/:mid' do
-	DB[:module].filter(:mid => params[:mid]).update(:opened => 'off')
+get '/admin/module/opened/:opened/:mid' do
+	opened = params[:opened] == 'on' ? 'on' : 'off'
+	DB[:module].filter(:mid => params[:mid]).update(:opened => opened)
 	redirect back
 end
