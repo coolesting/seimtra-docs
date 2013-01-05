15b0c4bfe2dab8c24b5721e9f9bc1f547ead76ef
coolesting <coolesting@gmail.com>
2013-01-05 00:57:00 -0800

    update the editor

--- modules/system/applications/file.rb
+++ modules/system/applications/file.rb
@@ -9,6 +9,24 @@ post '/_upload' do
 
 end
 
+get '/_folder' do
+
+	ds = DB[:_file].filter(:uid => _user[:uid])
+
+	unless ds.empty?
+		require 'json'
+		JSON.pretty_generate ds.select(:fid, :name, :filetype).reverse_order(:fid).limit(9).all
+	else
+		nil
+	end
+
+end
+
+get '/_file/:fid' do
+	ds = DB[:_file].filter(:fid => params[:fid].split('.').first)
+	send_file settings.upload_path + ds.get(:path), :type => ds.get(:filetype).to_sym
+end
+
 helpers do
 	
 	#save file info to db
@@ -19,17 +37,17 @@ helpers do
 	def _file_save file
 		fields = {}
 		fields[:uid] 		= _user[:uid]
-		fields[:name] 		= file[:filename]
+		fields[:name] 		= file[:filename].split('.').first
 		fields[:created]	= Time.now
-		fields[:path] 		= settings.upload_path + "/#{user[:uid]}-" + fields[:created].strftime("%s") + "-#{file[:filename]}"
+		fields[:path] 		= "/#{_user[:uid]}-" + fields[:created].strftime("%s") + "-#{file[:filename]}"
 		filetype			= file[:filename].split('.').last
-		fields[:filetype]	= filetype if _vars(:filetype).include? filetype.to_sym
+		fields[:filetype]	= filetype if _vars(:filetype).include? filetype
 
 		table = file[:table] ? file[:table].to_sym : :_file
 		DB[table].insert(fields)
 
 		file_content = file[:tempfile].read
-		File.open(fields[:path], 'w+') do | f |
+		File.open(settings.upload_path + fields[:path], 'w+') do | f |
 			f.write file_content
 		end
 	end
